75a3f3f84dd7cf7d266f2f66d841d8768afab0cb
Kaazam <kaazam1@o2.pl>
2022-07-30 15:47:02 +0200

    zmiana server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Testowanie jest super SUPER!');
+  res.end('jebie mnie to .com.pl (etnie)');
 }
 
 const server = http.createServer(requestListener);
